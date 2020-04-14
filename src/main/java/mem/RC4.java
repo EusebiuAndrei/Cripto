831e311b4967e3c273b3d1f79108f39abf154967
Eusebiu Gagea <eusebiuandrei_gagea@yahoo.co.uk>
2020-04-15 00:16:02 +0300

    fds

--- src/main/java/mem/RC4.java
+++ src/main/java/mem/RC4.java
@@ -1,46 +1,72 @@
 package mem;
 
-public class RC4 {
-    private final byte[] S = new byte[256];
-    private final byte[] T = new byte[256];
+import java.util.Random;
+
+public class RC4 implements NumberGenerator {
+    private final int[] S = new int[256];
+    private final int[] T = new int[256];
     private final int keylen;
 
-    public RC4(final byte[] key) {
+    public void displayArray(int[] arr) {
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+
+        System.out.println();
+    }
+
+    public RC4() {
+        int[] key = getRandomKey();
         keylen = key.length;
+        displayArray(key);
+
         for (int i = 0; i < 256; i++) {
-            S[i] = (byte) i;
+            S[i] = i;
             T[i] = key[i % keylen];
         }
 
-        int j = 0;
-        byte tmp;
+        int j = 0, tmp;
 
         for (int i = 0; i < 256; i++) {
-            j = (j + S[i] + T[i]) & 0xFF;
+            j = (j + S[i] + T[i]) % 256;
             tmp = S[j];
             S[j] = S[i];
             S[i] = tmp;
         }
+
+        displayArray(S);
     }
 
-    public byte[] encrypt(final byte[] plaintext) {
-        final byte[] ciphertext = new byte[plaintext.length];
-        int i = 0, j = 0, k, t;
-        byte tmp;
-        for (int counter = 0; counter < plaintext.length; counter++) {
-            i = (i + 1) & 0xFF;
-            j = (j + S[i]) & 0xFF;
-            tmp = S[j];
-            S[j] = S[i];
-            S[i] = tmp;
-            t = (S[i] + S[j]) & 0xFF;
-            k = S[t];
-            ciphertext[counter] = (byte) (plaintext[counter] ^ k);
+    public int trans(int i, int j) {
+        int tmp;
+        i = (i + 1) % 256;
+        j = (j + S[i]) % 256;
+        tmp = S[j];
+        S[j] = S[i];
+        S[i] = tmp;
+        return S[(S[i] + S[j]) % 256];
+    }
+
+    public void generate() {
+        int[] sir = new int[256];
+        int j = 0;
+
+        for(int i = 0; i < 256; i++) {
+            sir[i] = trans(i, j);
         }
-        return ciphertext;
+
+        displayArray(sir);
     }
 
-    public byte[] decrypt(final byte[] ciphertext) {
-        return encrypt(ciphertext);
+    private int[] getRandomKey() {
+        Random random = new Random();
+        int[] key = new int[16];
+
+        for(int i = 0; i < 16; i++) {
+            int bit = random.nextInt(2);
+            key[i] = bit;
+        }
+
+        return key;
     }
 }
